bdb9a41ad835fe0d0d3633995a6149b4a3d1462f
Victor Mikhaylov <victor.mikhaylov@epicgames.com>
2024-12-17 09:15:25 +0400

    Update Facade

--- src/main/java/com/epam/facade/OnStartServer.java
+++ src/main/java/com/epam/facade/OnStartServer.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
@@ -14,10 +15,15 @@ import org.springframework.stereotype.Component;
 public class OnStartServer implements ApplicationListener<ApplicationReadyEvent> {
 
     Logger logger = LoggerFactory.getLogger(OnStartServer.class);
+
     @Autowired
     private TraineeService traineeService;
+
     @Autowired
     private TraineeDao traineeDao;
+
+    @Autowired
+    private ApplicationContext applicationContext;
 
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
@@ -27,6 +33,10 @@ public class OnStartServer implements ApplicationListener<ApplicationReadyEvent>
         traineeDao.save(trainee1);
 
         Trainee fromMap = traineeDao.get(1);
-        logger.info("Trainee 1 : " + fromMap);
+        logger.info("Trainee 1: {}", fromMap);
+
+        for (String name : applicationContext.getBeanDefinitionNames()) {
+            //            System.out.println("bean: " + name);
+        }
     }
 }
